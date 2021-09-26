3d7f3de51f627fb042d75a0df3728b73b1b7fffe
Dana Lu <31543997+dana-lu@users.noreply.github.com>
2021-09-26 02:55:07 -0400

    remove double import

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
-import React, { useState, useEffect } from "react";
 import GrowerDashboardPage from "./GrowerDashboardPage";
 import SplashPage from "./SplashPage";
 
