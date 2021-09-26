bf6e54376b0ec1b1962a09ae4111d85ffe476124
Jafer Ahmed <jmahmed@usc.edu>
2021-09-25 20:47:05 -0700

    Added plant add button

--- app/src/GrowerPageBody.js
+++ app/src/GrowerPageBody.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./GrowerPageBody.css";
 import { Card } from "antd";
 import DashboardPlant from "./DashboardPlant";
+import { PlusCircleOutlined } from "@ant-design/icons";
 
 const { Meta } = Card;
 
@@ -11,8 +12,24 @@ function GrowerPageBody() {
       <div className="GrowerPageBody__plantsList">
         <h1>Your Plants</h1>
         <hr />
-
         <DashboardPlant />
+      </div>
+      <div className="GrowerPageBody__actions">
+        <h1>Actions</h1>
+        <Card
+          bordered={true}
+          style={{ width: 200, height: 100, backgroundColor: "#7FDA47" }}
+        >
+          <div
+            className="GrowerPageBodyActions__addPlantButton"
+            style={{ color: "white" }}
+          >
+            <span className="addPlantButton__icon">
+              <PlusCircleOutlined />
+            </span>
+            Add Plant
+          </div>
+        </Card>
       </div>
     </div>
   );
